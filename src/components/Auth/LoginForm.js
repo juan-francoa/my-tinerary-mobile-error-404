fdafdbb183f29385f4f7184c4c7ff64ff1ca179e
pilisalto <m.pilarsalto@gmail.com>
2022-12-09 05:59:31 -0300

    MT-M1-08

---
+++ src/components/Auth/LoginForm.js
@@ -0,0 +1,89 @@
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TextInput,
+  Button,
+  Keyboard,
+} from "react-native";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
+
+export default function LoginForm() {
+  const formik = useFormik({
+    initialValues: initialValues(),
+    validationSchema: Yup.object(validationSchema()),
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      console.log("Formulario enviado...");
+      console.log(formValue);
+    },
+  });
+
+  return (
+    <View>
+      <Text style={styles.title}>Login</Text>
+      <TextInput
+        placeholder="Email"
+        style={styles.input}
+        autoCapitalize="none"
+        value={formik.values.email}
+        onChangeText={(text) => formik.setFieldValue("email", text)}
+      />
+      <TextInput
+        placeholder="Password"
+        style={styles.input}
+        autoCapitalize="none"
+        secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue("password", text)}
+      />
+      <Button title="Entrar" onPress={formik.handleSubmit} />
+
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
+    </View>
+  );
+}
+
+function initialValues() {
+  return {
+    username: "",
+    password: "",
+  };
+}
+
+function validationSchema() {
+  return {
+    username: Yup.string().required("The user is required"),
+    password: Yup.string().required("Password is required"),
+  };
+}
+
+const styles = StyleSheet.create({
+  title: {
+    textAlign: "center",
+    fontSize: 28,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginBottom: 15,
+    color: "white"
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderRadius: 10,
+    color: "white",
+    borderColor: "white"
+  },
+  error: {
+    textAlign: "center",
+    color: "#f00",
+    marginTop: 20,
+    color: "white"
+  },
+});
---
+++ src/redux/reducers/rootReducer.js
@@ -0,0 +1,21 @@
+import citiesReducer from "./citiesReducer";
+import hotelsReducer from "./hotelsReducer";
+import myCitiesReducer from "./myCitiesReducer";
+import myHotelsReducer from "./myHotelsReducer";
+import myTinerariesReducer from "./myTinerariesReducer";
+import myShowsReducer from "./myShowsReducer";
+import usersReducer from "./usersReducer";
+import signInReducer from "./signInReducer"
+
+const rootReducer = {
+    citiesReducer,
+    hotelsReducer,
+    myCitiesReducer,
+    myHotelsReducer,
+    myTinerariesReducer,
+    myShowsReducer,
+    usersReducer,
+    signInReducer
+}
+
+export default rootReducer;
